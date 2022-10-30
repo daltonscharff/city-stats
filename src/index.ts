1e628ab671f116795993d1f67fe2c59294744735
Dalton Scharff <dalton.scharff@gmail.com>
2022-10-30 14:38:37 -0500

    implements commander for cli

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,49 @@
+import { getNumbeoData } from "./numbeo";
 import { getWalkScoreData } from "./walkScore";
 import { getWikipediaData } from "./wikipedia";
+import { program } from "commander";
 
-async function main() {
-    const city = process.argv[2];
-    const wikipediaDataPromise = getWikipediaData(city).catch((e) => {
-        console.error(`Could not get Wikipedia data: ${e}`);
-        process.exit(1);
-    });
-    const walkScoreDataPromise = getWalkScoreData(city).catch((e) => {
-        console.error(`Could not get WalkScore data: ${e}`);
-    });
+program
+    .option("-w, --walkScore", "show WalkScore table")
+    .option("-c, --climate", "show climate data table")
+    .argument(
+        "<city>",
+        'city to find stats for (e.g, "Austin, TX" or "Amsterdam, Netherlands")',
+    )
+    .action(async (city, options) => {
+        const wikipediaDataPromise = getWikipediaData(city).catch((e) => {
+            console.error(`Could not get Wikipedia data: ${e}`);
+            process.exit(1);
+        });
+        const walkScoreDataPromise = getWalkScoreData(city).catch((e) => {
+            console.error(`Could not get WalkScore data: ${e}`);
+        });
+        const numbeoDataPromise = getNumbeoData(city).catch((e) => {
+            console.error(`Could not get Numbeo data: ${e}`);
+        });
 
-    const [wikipediaData, walkScoreData] = await Promise.all([
-        wikipediaDataPromise,
-        walkScoreDataPromise,
-    ]);
+        const [wikipediaData, walkScoreData, numbeoData] = await Promise.all([
+            wikipediaDataPromise,
+            walkScoreDataPromise,
+            numbeoDataPromise,
+        ]);
 
-    const cityStats = {
-        ...wikipediaData,
-        walkScore: walkScoreData,
-    };
+        const cityStats = {
+            ...wikipediaData,
+            walkScore: walkScoreData,
+            costOfLiving: numbeoData,
+        };
 
-    printCityStats(cityStats);
+        console.log(JSON.stringify(cityStats, undefined, 2));
 
-    function printCityStats(stats: typeof cityStats) {
-        console.log(
-            JSON.stringify(
-                {
-                    city: stats.city,
-                    population: stats.population,
-                    elevation: stats.elevation,
-                    area: stats.area,
-                    averageWalkScore: stats.walkScore?.average,
-                },
-                undefined,
-                2,
-            ),
-        );
-
-        console.log("Climate Data");
-        console.table(stats.climateData);
-
-        if (stats.walkScore) {
-            console.log("WalkScore by Neighborhood");
-            console.table(stats.walkScore.byNeighborhood);
+        if (options.climate) {
+            console.log("Climate Data");
+            console.table(cityStats.climateData);
         }
-    }
-}
+        if (options.walkScore) {
+            console.log("WalkScore by Neighborhood");
+            console.table(cityStats.walkScore?.byNeighborhood);
+        }
+    });
 
-main();
+program.parse(process.argv);
