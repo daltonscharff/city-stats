505be425cacad1df5512fd2b67ba6064bf5d006f
Dalton Scharff <dalton.scharff@gmail.com>
2023-07-04 16:06:37 -0400

    print a json response from find command

--- city-stats-functions/cmd/find.go
+++ city-stats-functions/cmd/find.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/daltonscharff/city-stats/internal/models"
 	"github.com/daltonscharff/city-stats/internal/services"
 	"github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
@@ -40,19 +42,35 @@ func find(location string, flags FindFlags) {
 	wikipediaService := services.WikipediaService{client}
 	wikipediaResponse, err := wikipediaService.LocationSearch(location)
 	if err != nil {
-		fmt.Println("wikipediaService.LocationSearch error", err)
+		fmt.Println("wikipediaService.LocationSearch error:", err)
 	}
 
-	wikipediaLocation := fmt.Sprintf("%s, %s", wikipediaResponse.City, wikipediaResponse.State)
+	loc := models.Location{}
+	loc.New(wikipediaResponse.City, wikipediaResponse.State, "United States")
 
-	// walkscoreService := services.WalkscoreService{client}
-	// walkscoreResponse, err := walkscoreService.(location)
-	// if err != nil {
-	// 	fmt.Println("wikipediaService.LocationSearch error", err)
-	// }
+	walkscoreService := services.WalkscoreService{client}
+	walkscoreResponse, err := walkscoreService.LocationSearch(loc)
+	if err != nil {
+		fmt.Println("walkscoreService.LocationSearch error:", err)
+	}
 
-	// numbeoServices := services.NumbeoService{client}
+	numbeoService := services.NumbeoService{client}
+	numbeoResponse, err := numbeoService.LocationSearch(loc)
+	if err != nil {
+		fmt.Println("numbeoService.LocationSearch error:", err)
+	}
 
-	fmt.Println(wikipediaLocation)
+	response := map[string]any{
+		"location":  loc,
+		"wikipedia": wikipediaResponse,
+		"walkscore": walkscoreResponse,
+		"numbeo":    numbeoResponse,
+	}
 
+	json, err := json.MarshalIndent(response, "", "  ")
+	if err != nil {
+		fmt.Println("Error formatting json:", err)
+	}
+
+	fmt.Println(string(json))
 }
