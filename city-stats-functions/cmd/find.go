bb01497a8927f89edaf48169a7991b58ae86b4db
Dalton Scharff <dalton.scharff@gmail.com>
2023-07-04 16:39:17 -0400

    displays climate and walkscore tables

--- city-stats-functions/cmd/find.go
+++ city-stats-functions/cmd/find.go
@@ -3,16 +3,17 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/daltonscharff/city-stats/internal/models"
 	"github.com/daltonscharff/city-stats/internal/services"
 	"github.com/go-resty/resty/v2"
+	"github.com/jedib0t/go-pretty/table"
 	"github.com/spf13/cobra"
 )
 
 type FindFlags struct {
-	showClimate       bool
-	showNeighborhoods bool
+	showTables bool
 }
 
 var (
@@ -21,17 +22,13 @@ var (
 		Use:   "find <city>",
 		Short: "Find stats for a given city",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("climate", flags.showClimate)
-			fmt.Println("neighborhoods", flags.showNeighborhoods)
-			fmt.Println("args", len(args))
 			find(args[0], flags)
 		},
 	}
 )
 
 func init() {
-	findCmd.Flags().BoolVarP(&flags.showClimate, "climate", "c", false, "show climate data")
-	findCmd.Flags().BoolVarP(&flags.showNeighborhoods, "neighborhoods", "n", false, "show WalkScore for individual neighborhoods")
+	findCmd.Flags().BoolVarP(&flags.showTables, "tables", "t", false, "show tables for climate and neighborhood walkscore data")
 
 	rootCmd.AddCommand(findCmd)
 }
@@ -73,4 +70,45 @@ func find(location string, flags FindFlags) {
 	}
 
 	fmt.Println(string(json))
+
+	if flags.showTables {
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Year"})
+
+		for _, record := range wikipediaResponse.ClimateTable {
+			t.AppendRow([]interface{}{
+				record.Name,
+				record.Month[0],
+				record.Month[1],
+				record.Month[2],
+				record.Month[3],
+				record.Month[4],
+				record.Month[5],
+				record.Month[6],
+				record.Month[7],
+				record.Month[8],
+				record.Month[9],
+				record.Month[10],
+				record.Month[11],
+				record.Year,
+			})
+		}
+		t.Render()
+
+		tt := table.NewWriter()
+		tt.SetOutputMirror(os.Stdout)
+		tt.AppendHeader(table.Row{"Neighborhood", "Population", "Walk", "Transit", "Bike"})
+
+		for _, record := range walkscoreResponse.NeighborhoodScores {
+			tt.AppendRow([]interface{}{
+				record.Name,
+				record.Population,
+				record.Score.Walk,
+				record.Score.Transit,
+				record.Score.Bike,
+			})
+		}
+		tt.Render()
+	}
 }
--- city-stats-functions/go.mod
+++ city-stats-functions/go.mod
@@ -7,17 +7,24 @@ require github.com/aws/aws-lambda-go v1.41.0
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-openapi/errors v0.20.3 // indirect
+	github.com/go-openapi/strfmt v0.21.7 // indirect
 	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/h2non/gock v1.2.0 // indirect
 	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
@@ -26,6 +33,7 @@ require (
 	github.com/spf13/viper v1.16.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	go.mongodb.org/mongo-driver v1.11.3 // indirect
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/net v0.11.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
